ecafa55a66c69db6a8a9887d75a42a89dc4b9c2a
Alberto Russo <alberto.russo@tribalworldwide.co.uk>
2018-09-09 21:49:20 +0100

    implement webpack and hello wolrd test

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/templates/views/index.js',
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        publicPath: '/',
+        path: path.resolve(__dirname, 'dist')
+    },
+    performance: {
+       hints: false,
+    },
+    mode: 'production',
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'Alberto Russo - Exercise',
+            myPageHeader: 'Hello World',
+            template: './src/templates/views/index.html',
+            filename: './index.html'
+        })
+    ]
+}
